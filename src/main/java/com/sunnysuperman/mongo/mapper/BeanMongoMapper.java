b4e836e2454b37cde333bd5eb9f4a6185c6934dc
Leo Ji <jirenshi@congzhitech.com>
2018-10-10 19:50:29 +0800

    Add deserialize options

--- src/main/java/com/sunnysuperman/mongo/mapper/BeanMongoMapper.java
+++ src/main/java/com/sunnysuperman/mongo/mapper/BeanMongoMapper.java
@@ -2,11 +2,19 @@ package com.sunnysuperman.mongo.mapper;
 
 import org.bson.Document;
 
+import com.sunnysuperman.commons.bean.ParseBeanOptions;
 import com.sunnysuperman.mongo.MongoMapper;
 import com.sunnysuperman.repository.serialize.SerializeManager;
 
 public class BeanMongoMapper<T> implements MongoMapper<T> {
     private Class<T> clazz;
+    private ParseBeanOptions options;
+
+    public BeanMongoMapper(Class<T> clazz, ParseBeanOptions options) {
+        super();
+        this.clazz = clazz;
+        this.options = options;
+    }
 
     public BeanMongoMapper(Class<T> clazz) {
         super();
@@ -15,7 +23,7 @@ public class BeanMongoMapper<T> implements MongoMapper<T> {
 
     @Override
     public T map(Document doc) {
-        return SerializeManager.deserialize(doc, clazz);
+        return SerializeManager.deserialize(doc, clazz, options);
     }
 
 }
